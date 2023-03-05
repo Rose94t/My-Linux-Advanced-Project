a71d40ac45a5da7a2cf0a2f3d042af98a541f2a2
Havva Nur <havvanurtp@gmail.com>
2023-03-05 03:19:56 +0300

    linux advanced project second commit

--- Linux-101-Advanced-Project/First_Part/result2-codes.sh
+++ Linux-101-Advanced-Project/First_Part/result2-codes.sh
@@ -5,7 +5,7 @@ do
   read -p 'Please write your file location: ' FILE 
   if [[ $FILE == '/home/ec2-user/Linux-101-Advanced-Project/First_Part/event_history.csv' ]]
   then
-    grep -i "serdar" $FILE | grep -i "terminate" | grep -Eo "i-[a-zA-Z0-9]{17}" | sort | uniq > result1.txt
+    grep -i "serdar" $FILE | grep -i "terminate" | grep -Eo "i-[a-zA-Z0-9]{17}" | sort | uniq > result.txt
     echo "Your result is ready under the result1.txt file"
     break
   else [[ ! -f $FILE ]]
